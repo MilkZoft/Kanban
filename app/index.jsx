431407a282847fa0091fb5f6332cd2bb4e32801a
Carlos Santana <czantany@MacBook-Pro-de-Carlos.local>
2016-04-04 15:20:19 -0700

    First commit

---
+++ app/index.jsx
@@ -0,0 +1,7 @@
+import 'style!css!./main.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.querySelector('#app'));
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const npmInstallPlugin = require('npm-install-webpack-plugin');
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
+};
+
+process.env.BABEL_ENV = TARGET;
+
+const common = {
+    entry: {
+        app: PATHS.app
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css%/,
+                loaders: ['style!css', 'css'],
+                include: PATHS.app
+            },
+            {
+                test: /\.jsx?$/,
+                loader: ['babel'],
+                query: {
+                    cacheDirectory: true,
+                    presets: ['react', 'es2015', 'survivejs-kanban']
+                },
+                include: PATHS.app
+            }
+        ]
+    }
+};
+
+if (TARGET === 'start' || !TARGET) {
+    module.exports = merge(common, {
+        devtool: 'eval-source-map',
+        devServer: {
+            contentBase: PATHS.build,
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+            stats: 'errors-only',
+            host: process.env.HOST,
+            port: process.env.PORT
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new npmInstallPlugin({
+                save: true
+            })
+        ]
+    });
+}
+
+if (TARGET === 'build') {
+    module.exports = merge(common, {});
+}
